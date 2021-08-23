3e19fc323e09b17414b8027037029f511712aae8
shahin-max <82497397+shahin-max@users.noreply.github.com>
2021-08-23 15:44:47 +0530

    10 ms

--- solutions/179. Largest Number.java
+++ solutions/179. Largest Number.java
@@ -1,29 +1,10 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-  public ListNode insertionSortList(ListNode head) {
-    ListNode dummy = new ListNode(0);
-    ListNode curr = head;
-    ListNode prev = dummy;
-    ListNode next = null;
-    while (curr != null) {
-      next = curr.next;
-      while (prev.next != null && prev.next.val < curr.val) {
-        prev = prev.next;
-      }
-      curr.next = prev.next;
-      prev.next = curr;
-      prev = dummy;
-      curr = next;
-    }
-    return dummy.next; 
+  public String largestNumber(int[] nums) {
+    String largestNum = Arrays.stream(nums)
+        .boxed()
+        .map(String::valueOf)
+        .sorted((o1, o2) -> (o2 + o1).compareTo(o1 + o2))
+        .collect(Collectors.joining(""));
+    return largestNum.charAt(0) == '0' ? "0" : largestNum;
   }
 }
