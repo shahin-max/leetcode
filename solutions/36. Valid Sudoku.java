51dcaabd7f5892076539d1938156661b62044f48
shahin-max <82497397+shahin-max@users.noreply.github.com>
2021-08-24 18:15:40 +0530

    13 ms

--- solutions/36. Valid Sudoku.java
+++ solutions/36. Valid Sudoku.java
@@ -1,27 +1,18 @@
 class Solution {
-    public boolean isValidSudoku(char[][] board) {
-        HashSet<Character>[] rowSet = new HashSet[9];
-        HashSet<Character>[] colSet = new HashSet[9];
-        HashSet<Character>[] boxSet = new HashSet[9];
-        for (int i = 0; i < 9; i++) {
-            rowSet[i] = new HashSet<>();
-            colSet[i] = new HashSet<>();
-            boxSet[i] = new HashSet<>();
-        }
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.') {
-                    char val = board[i][j];
-                    int boxIdx = (i / 3) * 3 + j / 3;
-                    if (rowSet[i].contains(val) || colSet[j].contains(val) || boxSet[boxIdx].contains(val)) {
-                        return false;
-                    }
-                    rowSet[i].add(val);
-                    colSet[j].add(val);
-                    boxSet[boxIdx].add(val);
-                }
-            }
-        }
-        return true;
-    }
+  public boolean isValidSudoku(char[][] board) {
+    Set<String> seen = new HashSet<>();
+​
+    for (int i = 0; i < 9; ++i)
+      for (int j = 0; j < 9; ++j) {
+        if (board[i][j] == '.')
+          continue;
+        final char c = board[i][j];
+        if (!seen.add(c + "@row" + i) ||
+            !seen.add(c + "@col" + j) ||
+            !seen.add(c + "@box" + i / 3 + j / 3))
+          return false;
+      }
+​
+    return true;
+  }
 }
